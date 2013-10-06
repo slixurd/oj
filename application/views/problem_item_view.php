1cea9178c9b9ba04182cb1a2b4b89a569dcc657a
oujiangping <785992791@qq.com>
2013-10-06 11:26:14 +0800

    问题详情页改为动态

--- application/views/problem_item_view.php
+++ application/views/problem_item_view.php
@@ -2,11 +2,11 @@
 	<div class="container side-split">
 		<div class="side-appendix">
 			<div class="problem-appendix">
-				<div><span>题号</span><span>10000</span></div>
-				<div><span>时间限制</span><span>1000ns</span></div>
-				<div><span>内存限制</span><span>100M</span></div>
-				<div><span>通过总数</span><span>65535</span></div>
-				<div><span>提交总数</span><span>4294967295</span></div>
+				<div><span>题号</span><span><?php echo $problem['problemId'] ?></span></div>
+				<div><span>时间限制</span><span><?php echo $problem['timeLimit']."ms" ?></span></div>
+				<div><span>内存限制</span><span><?php echo $problem['memoryLimit']."M" ?></span></div>
+				<div><span>通过总数</span><span><?php echo $problem['accepted'] ?></span></div>
+				<div><span>提交总数</span><span><?php echo $problem['submit'] ?></span></div>
 			</div>
 			<div class="button-side">
 				<button class="common-button">提交</button>
@@ -15,53 +15,42 @@
 		</div>
 		<div class="side-main">
 			<div class="sub-header">
-				<h3>10000:解决A+B</h3>
+				<h3><?php echo $problem['title'] ?></h3>
 			</div>
 			<div class="problem-info">
 				<div>
 					<h4>问题描述</h4>
 					<span>
-						计算A+B计算A+B计算A+B计算A+B
-						计算A+B
-						计算A+B
-						计算A+B
-						计算A+B
-						计算A+B
-						计算A+B计算A+B计算A+B计算A+B计算A+B
-						计算A+B计算A+B计算A+B计算A+B计算A+B计算A+B计算A+B
-						计算A+B
-						计算A+B
-						计算A+B
-						计算A+B
+						<?php echo $problem['description'] ?>
 					</span>
 				</div>			
 				<div>
 					<h4>输入描述</h4>
 					<span>
-						只有两个数字
+						<?php echo $problem['input'] ?>
 					</span>
 				</div>			
 				<div>
 					<h4>输出描述</h4>
 					<span>
-						只有一个数字
+						<?php echo $problem['output'] ?>
 					</span>
 				</div>			
 				<div>
 					<h4>输入示例</h4>
-					<span>1+1</span>
+					<span><?php echo $problem['sampleInput'] ?></span>
 				</div>			
 				<div>
 					<h4>输出示例</h4>
-					<span>2</span>
+					<span><?php echo $problem['sampleOutput'] ?></span>
 				</div>			
 				<div>
 					<h4>Hint</h4>
-					<span>0</span>
+					<span><?php echo $problem['hint'] ?></span>
 				</div>			
 				<div>
 					<h4>来源</h4>
-					<span>无</span>
+					<span><?php echo $problem['source'] ?></span>
 				</div>			
 			</div>
 
